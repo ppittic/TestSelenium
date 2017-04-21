0155d62ed2ae19b93acd771dd568b29cbc4acfdc
ppittic <ppittic@yahoo.com>
2017-04-21 14:47:23 +0300

    TEST3

--- src/test/java/packt/selenium/chap3_9/SearchGoogleTest.java
+++ src/test/java/packt/selenium/chap3_9/SearchGoogleTest.java
@@ -23,7 +23,7 @@ public class SearchGoogleTest {
     public void setUp() throws Exception {
         driver = new FirefoxDriver();
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        //driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     }
     @After
     public void tearDown() throws Exception {
